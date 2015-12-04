cd00ee9d8b7c52217f55e360773bcc78ba1ed379
Remold Krol <remold.krol@everett.nl>
2015-12-04 14:51:09 +0100

    #30 add env and domain as parameters

--- scripts/gen_ocdemo_certs.sh
+++ scripts/gen_ocdemo_certs.sh
@@ -10,31 +10,38 @@ SECRET_VARS_FILE=../secrets/secret-vars.yml
 
 # ----- End configuration
 
+BASEDIR=$(dirname $0)
+BASENAME=$(basename $0)
+
 # ----- Input handling
-if [ $# -eq 0 ]
+if [ $# -lt 2 ]
   then
-    echo "INFO: No arguments supplied, using default secrets file" $SECRET_VARS_FILE
+    echo "INFO: No arguments supplied, syntax: $BASENAME environment domain [secret-vars-file]"
+    exit 1
 fi
 
-if [ $# -gt 1 ]
+if [ $# -eq 2 ]
   then
-    echo "ERROR: Only 1 argument expected please supply only the file containing the secrets, for example " $SECRET_VARS_FILE
+    echo "INFO: No secret file supplied, using default secrets file" $SECRET_VARS_FILE
+fi
+
+if [ $# -gt 3 ]
+  then
+     echo "ERROR: Only 2 or 3 arguments expected, syntax: $BASENAME environment domain [secret-vars-file]"
     exit 1
 fi
 # ----- End Input handing
 
-if [ $# -eq 1 ]
+if [ $# -eq 3 ]
   then
-    SECRET_VARS_FILE=$1
+    SECRET_VARS_FILE=$3
     echo "INFO: Using file " $SECRET_VARS_FILE " for containing the secrets."
 fi
 
 #set -x
 
-oc_env=`grep "^env: " $SECRET_VARS_FILE | cut -f2 -d: | xargs`
-oc_basedomain=`grep "^base_domain: " $SECRET_VARS_FILE | cut -f2 -d: | xargs  | sed "s/{{ env }}/$oc_env/g"`
-
-BASEDIR=$(dirname $0)
+oc_env=$1
+oc_basedomain=$2
 
 # create some directories for housekeeping
 mkdir -p $BASEDIR/oc_certs/ssl
