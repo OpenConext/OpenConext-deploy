b15a138cea212a536d599a2c418deea68cb1fe04
Bart Geesink <bart.geesink@surfnet.nl>
2017-10-23 11:33:06 +0200

    Mongo: Add script to set Mongo kernel variables

---
+++ roles/mongo/files/mongo_kernel_settings.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if test -f /sys/kernel/mm/transparent_hugepage/enabled; then
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+fi
+if test -f /sys/kernel/mm/transparent_hugepage/defrag; then
+echo never > /sys/kernel/mm/transparent_hugepage/defrag
+fi
