d9d640f12e7f86ddc12843b1f1b83ba7476e4761
Okke Harsta <oharsta@zilverline.com>
2018-11-27 14:27:07 +0100

    Docker does not resolve localhost

--- tests/travis-build.sh
+++ tests/travis-build.sh
@@ -38,6 +38,7 @@ docker run --detach                                         \
 	--add-host link.vm.openconext.org:127.0.0.1             \
 	--add-host oidc.vm.openconext.org:127.0.0.1             \
 	--add-host manage.vm.openconext.org:127.0.0.1           \
+	--add-host localhost:127.0.0.1                          \
 	--hostname test.openconext.org                          \
 	-e TERM=xterm                                           \
 	-e ANSIBLE_CONFIG=${ANSIBLE_CONFIG}                     \
