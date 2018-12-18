532b51fad42262ad14e7f48da8099972eb9a3596
Bart Geesink <bart.geesink@surfnet.nl>
2018-12-18 15:05:41 +0100

    Make sure that the docker build runs correctly with the new provisioning script

--- tests/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-ansible-playbook -i inventory/docker --extra-vars="secrets_file=secrets/vm.yml" provision-vm.yml
--- tests/travis-build.sh
+++ tests/travis-build.sh
@@ -6,13 +6,14 @@ set -e
 status=0
 
 ANSIBLE_CONFIG=/ansible/ansible.cfg
-ANSIBLE_PLAYBOOK=/ansible/provision-vm.yml
+ANSIBLE_PLAYBOOK=/ansible/provision.yml
 ANSIBLE_INVENTORY=/ansible/environments/docker/inventory
 ANSIBLE_SECRETS=/ansible/environments/vm/secrets/vm.yml
-
+ANSIBLE_PLAYBOOK_WRAPPER=/ansible/provision
+ANSIBLE_USER=vagrant
 
 # start docker container
-docker run --detach                                         \
+docker run --detach                                             \
 	-v "${PWD}":/ansible:rw                                 \
 	-v /sys/fs/cgroup:/sys/fs/cgroup:ro                     \
 	--privileged                                            \
@@ -63,12 +64,7 @@ echo "================================================================="
 echo "================================================================="
 echo
 
-docker exec -t ansible-test                                      \
-	ansible-playbook                                             \
-		-i $ANSIBLE_INVENTORY                                    \
-		-e secrets_file=$ANSIBLE_SECRETS                         \
-		$ANSIBLE_PLAYBOOK                                        \
-		--syntax-check
+docker exec -w /ansible -t ansible-test  /ansible/provision docker $ANSIBLE_USER $ANSIBLE_SECRETS --syntax-check
 
 echo
 echo "================================================================="
@@ -78,11 +74,7 @@ echo "================================================================="
 echo "================================================================="
 echo
 
-docker exec -t ansible-test                                      \
-	ansible-playbook                                             \
-		-i $ANSIBLE_INVENTORY                                    \
-		-e secrets_file=$ANSIBLE_SECRETS                         \
-		$ANSIBLE_PLAYBOOK
+docker exec -w /ansible -t ansible-test  /ansible/provision docker $ANSIBLE_USER $ANSIBLE_SECRETS	   
 
 echo
 echo "================================================================="
@@ -93,12 +85,7 @@ echo "================================================================="
 echo
 
 TMPOUT=$(mktemp)
-docker exec -t ansible-test                                      \
-	ansible-playbook                                             \
-		-i $ANSIBLE_INVENTORY                                    \
-		-e secrets_file=$ANSIBLE_SECRETS                         \
-		$ANSIBLE_PLAYBOOK                                        \
- | tee $TMPOUT
+docker exec -w /ansible -t ansible-test  /ansible/provision docker $ANSIBLE_USER $ANSIBLE_SECRETS  | tee $TMPOUT
 
 echo
 echo "================================================================="
