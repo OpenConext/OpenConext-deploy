d2b93f5c20716844b75b0842d398342a302db237
Bart Geesink <bart.geesink@surfnet.nl>
2019-03-27 20:03:30 +0100

    This adds the ability to parse the log_logins to a database and analyze them further to create a table with the last time someone logged in. This is needed for the user lifecycle application, which reads from this database.

---
+++ roles/rsyslog/files/log_logins.sql
@@ -0,0 +1,26 @@
+CREATE TABLE  IF NOT EXISTS  `log_logins` (
+  `loginstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `userid` varchar(1000) NOT NULL,
+  `spentityid` varchar(1000) DEFAULT NULL,
+  `idpentityid` varchar(1000) DEFAULT NULL,
+  `trustedproxyentityid` varchar(1000) DEFAULT NULL,
+  `keyid` varchar(50) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sessionid` varchar(50) DEFAULT NULL,
+  `requestid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_index` (`sessionid`,`requestid`),
+  KEY `loginstamp_index` (`loginstamp`),
+  KEY `idpentityid_index` (`idpentityid`(255)),
+  KEY `spentityid_index` (`spentityid`(255)),
+  KEY `keyid_index` (`keyid`,`loginstamp`,`spentityid`(255)),
+  KEY `userid_idp_index` (`userid`(128),`idpentityid`(64))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ CREATE TABLE IF NOT EXISTS `last_login` (
+  `userid` varchar(255) NOT NULL,
+  `lastseen` date DEFAULT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `idx_user` (`userid`),
+  KEY `idx_lastseen` (`lastseen`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
