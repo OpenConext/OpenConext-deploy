a06b2598120cde55737fb020d2ff8f5e5dffcd5c
Thijs Kinkhorst <thijs@kinkhorst.com>
2021-01-05 19:44:53 +0100

    Remove roles/{authz*,oidc,tomcat*}

--- scripts/prep-dev-env.sh
+++ scripts/prep-dev-env.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-echo "Restarting php-fpm, httpd and tomcat"
+echo "Restarting php-fpm, httpd"
 sudo service php72-php-fpm restart
 sudo service httpd restart
-sudo service tomcat restart
 
 echo "Preparing dev env"
 (cd /opt/openconext/OpenConext-engineblock && php72 $(which composer) prepare-env)
