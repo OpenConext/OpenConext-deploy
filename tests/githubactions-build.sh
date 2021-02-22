bfeb45b3fffe50fd8707bca8001ea6ab4da71fc3
Bart Geesink <bart.geesink@surf.nl>
2021-02-22 11:04:51 +0100

    GitHub actions script: Remove prepenv and just use the vm environment. Makes integration a lot easier

--- tests/githubactions-build.sh
+++ tests/githubactions-build.sh
@@ -50,10 +50,13 @@ EOF
 
 # Prepare the environment
 echo "Prepping the environment" 
+mkdir -p environments-external
 cp -r environments/vm/ environments-external/github
+mv environments-external/github/group_vars/vm.yml environments-external/github/group_vars/github.yml
 
 # Change the hostname in the inventory
 cp environments/template/inventory environments-external/github/
+sed -i 's/%env%/github/g' environments-external/github/inventory
 sed -i 's/%target_host%/ansible-test-ga ansible_connection=docker/g' environments-external/github/inventory 
 
 # Create the proper host_vars file
