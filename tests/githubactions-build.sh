6b5e014f607423d794a1e4a925841a4bfadd0c53
Bart Geesink <bart.geesink@surf.nl>
2021-02-22 13:47:02 +0100

    Make sure /bin/cp is called

--- tests/githubactions-build.sh
+++ tests/githubactions-build.sh
@@ -52,17 +52,17 @@ EOF
 # Prepare the environment
 echo "Prepping the environment" 
 mkdir -p environments-external
-cp -r environments/vm/ environments-external/github
-mv environments-external/github/group_vars/vm.yml environments-external/github/group_vars/github.yml
+/bin/cp -r environments/vm/ environments-external/github
+/bin/mv environments-external/github/group_vars/vm.yml environments-external/github/group_vars/github.yml
 sed -i 's/192.168.66.98/127.0.0.1/g' environments-external/github/group_vars/github.yml
 sed -i 's/192.168.66.99/127.0.0.1/g' environments-external/github/group_vars/github.yml
 # Change the hostname in the inventory
-cp environments/template/inventory environments-external/github/
+/bin/cp environments/template/inventory environments-external/github/
 sed -i 's/%env%/github/g' environments-external/github/inventory
 sed -i 's/%target_host%/ansible-test-ga ansible_connection=docker/g' environments-external/github/inventory 
 
 # Create the proper host_vars file
-cp environments/template/host_vars/template.yml environments-external/github/host_vars/ansible-test-ga.yml
+/bin/cp environments/template/host_vars/template.yml environments-external/github/host_vars/ansible-test-ga.yml
 
 echo
 echo "================================================================="
