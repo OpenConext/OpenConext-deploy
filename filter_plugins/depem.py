8e727f464334ce6674337fac1eaad097983c419d
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2025-03-05 13:23:37 +0100

    clean up

--- filter_plugins/depem.py
+++ filter_plugins/depem.py
@@ -2,11 +2,14 @@
 # Usage: {{ foo | depem }}
 
 def depem(string):
-  import re
-  return re.sub(r'\s+|(-----(BEGIN|END).*-----)', '', string)
+    import re
+
+    return re.sub(r'\s+|(-----(BEGIN|END).*-----)', '', string)
+
 
 class FilterModule(object):
-  def filters(self):
-    return {
-      'depem': depem,
-    }
+    @staticmethod
+    def filters():
+        return {
+            'depem': depem,
+        }
